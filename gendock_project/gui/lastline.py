0c345ddb3ee7a5bc56eb456d17aadbdc91365b86
shayanshd <shayanshad@gmail.com>
2024-01-12 14:24:24 +0330

    minor fix

--- gendock_project/gui/lastline.py
+++
@@ -1,4 +0,0 @@
-import os
-
-path = '/home/shayan/Documents/gendock/gendock_project/receptor_files/6lu7.pdbqt'
-print(os.path.abspath(path))
--- gendock_project/rest/lstm_chem/utils/config.py
+++ gendock_project/rest/lstm_chem/utils/config.py
@@ -1,13 +1,13 @@
 import os
 import time
 import json
-from bunch import Bunch
+from munch import Munch
 
 
 def get_config_from_json(json_file):
     with open(json_file, 'r') as config_file:
         config_dict = json.load(config_file)
-    config = Bunch(config_dict)
+    config = Munch(config_dict)
     return config
 
 
